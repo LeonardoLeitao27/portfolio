d85f0c4d5735622de522427ad3f1b0c207d445fa
Leonardo <leoleitao27@gmail.com>
2022-05-07 17:15:13 -0300

    flex box e dark mode aplicado

--- src/js/script-barra.js
+++ src/js/script-barra.js
@@ -3,6 +3,8 @@ const body = document.querySelector('body'),
       toggle = body.querySelector(".toggle"),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
+      iconeH = document.getElementsByClassName("bloco-habilidades");
+      projetos = document.getElementsByClassName("blocosproj");
 
 
 toggle.addEventListener("click" , () =>{
@@ -13,6 +15,14 @@ toggle.addEventListener("click" , () =>{
 
 modeSwitch.addEventListener("click" , () =>{
     body.classList.toggle("dark");
+
+    for (let i = 0; i < iconeH.length; i++) {
+        iconeH[i].classList.toggle("fundo-bloco");
+      }
+
+    for (let i = 0; i < projetos.length; i++) {
+        projetos[i].classList.toggle("fundo-bloco");
+      }
     
     if(body.classList.contains("dark")){
         modeText.innerText = "Light mode";
